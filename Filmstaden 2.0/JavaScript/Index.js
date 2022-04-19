ba59b7dd20c5bcd127ca0a03453af48f45e20ff7
Johannes Pettersson <0319pejo@jonkoping.se>
2022-04-19 14:27:13 +0200

    navbar

--- Filmstaden 2.0/JavaScript/Index.js
+++ Filmstaden 2.0/JavaScript/Index.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
     
-    
+    $(".navlink").mouseenter(function(){
+        anime({
+            targets: this,
+            scale: 1.2,
+            duration: 600,
+            elasticity: 400
+          });
+    });
+
+    $(".navlink").mouseleave(function(){
+        anime({
+            targets: this,
+            scale: 1.0,
+            duration: 600,
+            elasticity: 300
+          });
+    });
 
 });
-
