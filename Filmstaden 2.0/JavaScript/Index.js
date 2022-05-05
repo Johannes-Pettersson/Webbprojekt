ef567bae0fd1140202559c2ecf4dcd597c849729
Johannes Pettersson <0319pejo@jonkoping.se>
2022-05-05 12:00:18 +0200

    sliders

--- Filmstaden 2.0/JavaScript/Index.js
+++ Filmstaden 2.0/JavaScript/Index.js
@@ -26,8 +26,207 @@ $(document).ready(function(){
           });
     }
 
+    let slider1State = 1;
+    let slider2State = 1;
+    let slider3State = 1;
+    let slider4State = 1;
+
+    $('.arrowLeft').click(function(e){
+
+        e.preventDefault();
+
+        switch($(this).parent().parent().attr("id")){
+            case "slider1":
+                switch(slider1State){
+                    case 1:
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide1").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage1.png');
+
+                        slider1State--;
+                        break;
+                    case 3:
+                        $(this).parent().parent().find("#slide3").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider1State--;
+                        break;
+                }
+                break;
+            case "slider2":
+                switch(slider2State){
+                    case 1:
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide1").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage1.png');
+
+                        slider2State--;
+                        break;
+                    case 3:
+                        $(this).parent().parent().find("#slide3").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider2State--;
+                        break;
+                }
+                break;
+            case "slider3":
+               switch(slider3State){
+                    case 1:
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide1").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage1.png');
 
 
+                        slider3State--;
+                        break;
+                    case 3:
+                        $(this).parent().parent().find("#slide3").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+
+                        slider3State--;
+                        break;
+                }
+                break; 
+            case "slider4":
+
+                switch(slider4State){
+                    case 1:
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide1").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage1.png');
+
+
+                        slider4State--;
+                        break;
+                    case 3:
+                        $(this).parent().parent().find("#slide3").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+
+                        slider4State--;
+                        break;
+                }
+                break;
+        }
+    });
+
+    $('.arrowRight').click(function(e){
+
+        e.preventDefault();
+        
+        switch($(this).parent().parent().attr("id")){
+            case "slider1":
+                switch(slider1State){
+                    case 1:
+                        $(this).parent().parent().find("#slide1").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider1State++;
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide3").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage3.png');
+
+                        slider1State++;
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+            case "slider2":
+                switch(slider2State){
+                    case 1:
+                        $(this).parent().parent().find("#slide1").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider2State++;
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide3").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage3.png');
+
+                        slider2State++;
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+            case "slider3":
+                switch(slider3State){
+                    case 1:
+                        $(this).parent().parent().find("#slide1").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider3State++;
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide3").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage3.png');
+
+                        slider3State++;
+                        break;
+                    case 3:
+                        break;
+                }
+                break; 
+            case "slider4":
+                switch(slider4State){
+                    case 1:
+                        $(this).parent().parent().find("#slide1").addClass("hidden");
+                        $(this).parent().parent().find("#slide2").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage2.png');
+
+                        slider4State++;
+                        break;
+                    case 2:
+                        $(this).parent().parent().find("#slide2").addClass("hidden");
+                        $(this).parent().parent().find("#slide3").removeClass("hidden");
+
+                        $(this).parent().find('.stageShower').attr('src', 'Assets/scrollstage3.png');
+
+                        slider4State++;
+                        break;
+                    case 3:
+                        break;
+                }
+                break;
+        }
+    });
 
     /*
     $('img').click(function(){
